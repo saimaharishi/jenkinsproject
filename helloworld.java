c19a01976aff28fb4cc288063b2a7175c35e283f
saimaharishi <138868169+saimaharishi@users.noreply.github.com>
2023-09-07 15:09:56 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class helloworld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello Agni");
