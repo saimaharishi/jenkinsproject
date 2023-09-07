7175abdefdaa24c4cb45752ad5d7e4df0d8a4294
saimaharishi <138868169+saimaharishi@users.noreply.github.com>
2023-09-07 17:12:25 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -6,13 +6,6 @@ System.out.println("Hello Jal");
 System.out.println("Hello Prithvi");
 System.out.println("Hello Aakash");
 System.out.println("Hello Vaayu");
-System.out.println("Hello Vaayu");
-System.out.println("THANK YOU UNIVERSE");
-      System.out.println("Hello Vaayu");
-System.out.println("Hello Vaayu");
-System.out.println("THANK YOU UNIVERSE");
-      System.out.println("Hello Aakash");
-
 System.out.println("THANK YOU UNIVERSE");
       System.out.println("Hello Vaayu");
 System.out.println("Hello Vaayu");
