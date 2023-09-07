c122859905fe0b85862439a30896dafd26d95429
saimaharishi <mahashree.g@gmail.com>
2023-09-07 14:22:45 +0530

    adding helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello Agni");
+System.out.println("Hello Jal");
+System.out.println("Hello Prithvi");
+System.out.println("Hello Aakash");
+System.out.println("Hello Vaayu");
+System.out.println("THANK YOU UNIVERSE");
+   }
+}
